ebef42c5933432d24e85604e95311feb7c09a4d2
Luning ZHU <luningzhu@Luning-de-MacBook-Pro.local>
2025-03-13 15:07:51 +0100

    add parameters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
+#include<vector>
+
+
+
+struct parameters
+{
+    //func
+    //grad_func
+    unsigned int n;
+    std::vector<double> x0;
+    double eps_r;
+    double eps_s;
+    unsigned k_max;
+    std::vector<bool> stopping_condition[3],lr_uploate_method[3];
+    double int_alpha,sigma;
+};
+
 
 int main(){
     
     return 0;
+
 }
