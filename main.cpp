71836997d324b05a56babcc12718bc137644974c
AleCava01 <52034469+AleCava01@users.noreply.github.com>
2025-03-14 17:39:06 +0100

    added vector handling: norm1 and vec_subtract functions

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include<iostream>
 #include<vector>
 #include<cmath>
+#include <functional>
 
-
-struct func{
-    double value(){};
-};
 
 struct parameters
 {
-    //func
-    //grad_func
+    std::function<double(double)> func;
+    std::function<double(double)> grad_func;
     unsigned int n; // dimension
     std::vector<double> x0; // initial guess
     double eps_r; // residual tolerance
@@ -20,9 +17,40 @@ struct parameters
     double alpha_zero; // initial learning rate
 };
 
-double eval(parameters params){ return 0.0; }; // must become a function template, see 1.1 Suggestion, first bullet point
+std::vector<double> vec_subtract(std::vector<double> &v1, std::vector<double> &v2){
+    std::vector<double> result;
+    for(unsigned i=0; i<v1.size(); ++i){
+        result[i]= v1[i] - v2[i];
+    }
+    return result;
+};
+
+double norm1(const std::vector<double>& vec){
+    double norm;
+    for(unsigned i=0; i<vec.size(); ++i){
+        norm += std::abs(vec[i]);
+    }
+    return norm;
+};
+
+std::vector<double> eval(const parameters& params){ 
+    double alpha_k;
+    std::vector<double> x = params.x0;
+    bool stopping_conditon;
+    std::vector<double> x_old;
+    x_old = x;
+    x_old[0]-=(1+params.eps_s);
+    while(norm1(vec_subtract(x,x_old))>params.eps_s){
+        x_old = x;
+        // do stuff
+        std::cout<< "porcodio" << std::endl;
+    }
+    return x;
+
+}; // must become a function template, see 1.1 Suggestion, first bullet point
 
 // eval function with exponential decay
+
 
 // eval function with inverse decay
 
@@ -37,7 +65,8 @@ int main(){
     auto f = [](double x[2]) {return 2*x[0]*x[1] + 4*std::pow(x[0],4)+2*std::pow(x[1],2)+2*x[0];};
     double x0[2] = {0.4,4.9};
     std::cout << f(x0) << std::endl;
-
+    
+    
 
     return 0;
 
